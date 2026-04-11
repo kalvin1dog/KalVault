4ac1f81285a86e69350ba58173995dc833a4562c
Kalvin Ernst <kalvinlernst@gmail.com>
2026-04-12 09:40:43 +1000

    Add files via upload

---
+++ js/DownloadFile.js
@@ -0,0 +1,11 @@
+﻿window.downloadFile = (fileName, base64Data) => {
+        const link = document.createElement('a');
+        link.download = fileName;
+    //link.href = "data:text/csv;base64," + base64Data;
+    link.href = "data:text/csv;charset=utf-8," + base64Data;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+};
+
+
